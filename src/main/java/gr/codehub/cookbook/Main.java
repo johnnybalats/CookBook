3a924a226f0ab8f846c22553e52b980741d206f6
John Balatsos <westcoast1910@hotmail.com>
2020-08-30 17:42:06 +0300

    - Fix Main method - Add methods: testCookBook(), testRecipeWords()

--- src/main/java/gr/codehub/cookbook/Main.java
+++ src/main/java/gr/codehub/cookbook/Main.java
@@ -1,13 +1,26 @@
 package gr.codehub.cookbook;
 
 import gr.codehub.cookbook.exceptions.BusinessException;
+import gr.codehub.cookbook.model.CookBook;
+import gr.codehub.cookbook.model.Ingredient;
 import gr.codehub.cookbook.model.Recipe;
+import gr.codehub.cookbook.service.CookBookService;
 
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n----- Testing Read File -----");
+        testReadFile();
+        System.out.println("\n----- Testing Cook Book -----");
+        testCookBook();
+        System.out.println("\n----- Testing Recipe Words -----");
+        testRecipeWords();
+    }
 
     private static  String readFile(File file) throws BusinessException {
 
@@ -26,84 +39,83 @@ public class Main {
 
     private static void testReadFile() {
         try {
-            readFile(new File("difgywdoigvywiogyvceiwygvpvye.ce9igyiyg"));
+            readFile(new File("something.smth"));
         } catch (BusinessException e) {
             System.out.println("Business error: cannot see the contents of this file");
         }
     }
 
-    public static void main(String[] args) {
+    private static void testCookBook() {
 
-        testReadFile();
+        CookBook cookBook = new CookBookService().createCookBook();
 
+        List<Recipe> allRecipes = cookBook.getRecipes();
+        System.out.println("\nThe book contains " + allRecipes.size() + " recipes");
+        for (Recipe recipe: allRecipes)
+            System.out.println("Recipe = " + recipe.getName());
 
-//        Recipe r1 = new Recipe();
-//        r1.setName("Boiled eggs");
-//        Recipe r2 = new Recipe();
-//        r2.setName("Fried eggs");
-//        Recipe r3 = new Recipe();
-//        r3.setName("Poached eggs");
-//
-//        Recipe[] recipeArray = new Recipe[15];
-//        recipeArray[0] = r1;
-//
-//        List<Recipe> recipeList = new ArrayList<>();
-//        recipeList.add(r1);
-//        recipeList.add(r2);
-//        recipeList.add(r3);
-//        recipeList.add(r1);
-//        recipeList.add(r2);
-//        recipeList.add(r3);
-//
-//        System.out.println(recipeList.size());
-//
-//        Set<Recipe> set1 = new HashSet<>();
-//        set1.addAll(recipeList);
-//        System.out.println(set1.size());
-//
-//        List<Recipe> listFromSet = new ArrayList<>();
-//        listFromSet.addAll(set1);
-//
-//        listFromSet.forEach(System.out::println);
+        List<Ingredient> allIngredients = new ArrayList<>();
+        for (Recipe recipe: allRecipes)
+            allIngredients.addAll(recipe.getIngredients());
+        System.out.println("\nThe book recipes uses " + allIngredients.size() + " ingredients (with quantities)");
+        for (Ingredient ingredient: allIngredients)
+            System.out.println("Ingredient = " + ingredient.getName() + ", " + ingredient.getQuantity());
+
+        Set<Ingredient> allUniqueIngredients = new HashSet<>(allIngredients);
+        System.out.println("\nThe book recipes uses " + allUniqueIngredients.size() + " unique ingredients (with quantities)");
+        for (Ingredient ingredient: allUniqueIngredients)
+            System.out.println("Unique ingredient = " + ingredient.getName() + ", " + ingredient.getQuantity());
+
+        Set<String> allIngredientTypes = new HashSet<>();
+        for (Ingredient ingredient: allIngredients)
+            allIngredientTypes.add(ingredient.getName());
+        System.out.println("\nThe book recipes uses " + allIngredientTypes.size() + " unique ingredient types");
+        for (String name: allIngredientTypes)
+            System.out.println("Unique ingredient type = " + name);
+    }
+
+    private static void testRecipeWords() {
 
         String recipeText = "ena aygo mia koutalia zaxari mia koutalia aleyri kai ena akoma aygo";
-        String[] words = recipeText.split(" ");
-        List<String> wordList = Arrays.asList(words);
+        String[] wordsArray = recipeText.split(" ");
+        List<String> wordsList = Arrays.asList(wordsArray);
 
-        List<String> longWords = wordList
+        List<String> longWords = wordsList
                 .stream()
                 .filter(word -> word.length() > 3)
                 .collect(Collectors.toList());
 
+        System.out.println("\nPrint long words with for: ");
+        for (String word: longWords)
+            System.out.println(word);
 
+        System.out.println("\nPrint long words with forEach: ");
         longWords.forEach(System.out::println);
 
-//        System.out.println("");
-//
-//        for (String word: longWords)
-//            System.out.println(word);
-//
-//
-//        Map<String, Integer> wordMap = new HashMap<>();
-//
-//        for (String word: words) {
-//
-//            Integer wordCount = wordMap.get(word);
-//
-//            if (wordCount == null)
-//                wordCount = 0;
-//
-//            wordCount++;
-//
-//            wordMap.put(word, wordCount);
-//        }
-//
-//        System.out.println("");
-//
-//        wordMap
-//                .forEach((name, count) -> System.out.println(name + " = " + count));
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        for (String word: wordsList) {
 
-//        for (Map.Entry entry: wordMap.entrySet())
-//            System.out.println(entry.getKey() + " = " + entry.getValue());
+            Integer wordCount = wordCountMap.get(word);
+            if (wordCount == null) {
+                wordCount = 0;
+            }
+            wordCount++;
+            wordCountMap.put(word, wordCount);
+        }
+
+        System.out.println("\nPrint word count with forEach: ");
+        wordCountMap.forEach((name, count) -> System.out.println(name + " = " + count));
+
+        System.out.println("\nPrint words from Map: ");
+        for (String s: wordCountMap.keySet())
+            System.out.println(s);
+
+        System.out.println("\nPrint counts from Map: ");
+        for (Integer s: wordCountMap.values())
+            System.out.println(s);
+
+        System.out.println("\nPrint words and counts from Map: ");
+        for (Map.Entry entry: wordCountMap.entrySet())
+            System.out.println(entry.getKey() + " = " + entry.getValue());
     }
 }
